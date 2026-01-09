fed377b0aa1b5a7698eeefab7f183f2b0f8eaa7b
guille15j <guillermosantosanchez@gmail.com>
2026-01-09 12:52:53 +0100

    Alumno A-Guillermo: Imeplementado ver-tareas con colores.

--- alumno_a/funciones.py
+++ alumno_a/funciones.py
@@ -7,8 +7,29 @@ def mostrar_menu():
 
 def ver_tareas(fichero):
     """Muestra todas las tareas numeradas."""
-    # TODO: Implementar
-    pass
+    try:
+        with open(fichero, "r", encoding="utf-8") as f:
+            lineas = f.readlines()
+        
+        if not lineas:
+            print(f"{Fore.BLUE}No hay tareas en el fichero.{Style.RESET_ALL}")
+            return
+
+        i = 1
+        for l in lineas:
+            secciones = l.split('|')
+            tarea = secciones[1].strip()
+
+            if secciones[0] == '1':
+                print(f"{i}. {Fore.CYAN}[ ]{Style.RESET_ALL} {tarea}")
+            else:
+                print(f"{i}. {Fore.GREEN}[✓]{Style.RESET_ALL} {tarea}")
+
+            i += 1
+
+    except FileNotFoundError:
+        print(f"{Fore.RED}El archivo no existe todavía.{Style.RESET_ALL}")
+
 
 def añadir_tarea(fichero):
     """Añade una nueva tarea al fichero."""
