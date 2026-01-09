2272be86783568d49f06b9d771528dd6791ab23d
Fernanda Gonzalez <sihayclasehoy@gmail.com>
2026-01-09 20:47:26 +0100

    Add import for os module

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from colorama import init, Fore, Style
 from alumno_a.funciones import mostrar_menu, ver_tareas, añadir_tarea
 from alumno_b.funciones import marcar_completada, eliminar_tarea, despedida
+import os
 
 # Menu funcional
 init()
