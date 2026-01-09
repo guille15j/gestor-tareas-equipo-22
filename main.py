f4a1b6ec6e34a727a01361964459d562af09abb5
guille15j <guillermosantosanchez@gmail.com>
2026-01-09 20:00:29 +0100

    Alumo A - Guillermo: Creacion del codigo main

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ from alumno_a.funciones import mostrar_menu, ver_tareas, añadir_tarea
 from alumno_b.funciones import marcar_completada, eliminar_tarea, despedida
 
 # Menu funcional
+init()
+
+def main():
+    fichero = "tareas.txt"
+    
+    while True:
+        opcion = mostrar_menu()
+
+        if opcion == "1":
+            ver_tareas(fichero)
+        elif opcion == "2":
+            añadir_tarea(fichero)
+        elif opcion == "3":
+            marcar_completada(fichero)
+        elif opcion == "4":
+            eliminar_tarea(fichero)
+        elif opcion == "5":
+            despedida()
+            break 
+
+if __name__ == "__main__":
+    main()
